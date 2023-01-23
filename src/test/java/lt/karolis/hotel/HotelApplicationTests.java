83a84e3ed86dc540720ba7320b03d76188306ccf
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-23 13:00:19 +0200

    Initial commit

---
+++ src/test/java/lt/karolis/hotel/HotelApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.karolis.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
