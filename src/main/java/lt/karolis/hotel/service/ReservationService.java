da10a6338476997f2cb6ac44f130a48833550219
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-24 10:26:47 +0200

    Add ReservationService class with data

---
+++ src/main/java/lt/karolis/hotel/service/ReservationService.java
@@ -0,0 +1,24 @@
+package lt.karolis.hotel.service;
+
+import lt.karolis.hotel.repository.model.Reservation;
+import lt.karolis.hotel.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReservationService {
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+
+    public Reservation getReservationById(int id) {
+        return reservationRepository.findById(id).get();
+    }
+
+    public List<Reservation> getQueryAllCustomers(Integer customerId) {
+        return reservationRepository.getAllCustomers(customerId);
+    }
+}
