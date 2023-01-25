1b9625e82cb7e544e43d6b02047d8a532bbda921
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-26 00:25:25 +0200

    Cleared unnecessary method in ReservationController

--- src/main/java/lt/karolis/hotel/controller/ReservationController.java
+++ src/main/java/lt/karolis/hotel/controller/ReservationController.java
@@ -1,14 +1,9 @@
 package lt.karolis.hotel.controller;
 
-import lt.karolis.hotel.repository.model.Reservation;
 import lt.karolis.hotel.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller //http://localhost:8080/
 @RequestMapping(path = "/reservationmapping") //http://localhost:8080/reservationmapping
@@ -18,9 +13,4 @@ public class ReservationController {
     @Autowired
     private ReservationService reservationService;
 
-    @GetMapping(path = "/allcustomers") //http://localhost:8080/reservationmapping/allcustomers
-
-    public @ResponseBody List<Reservation> getQuerryAllCustomers() {
-        return reservationService.getQueryAllCustomers();
-    }
 }
