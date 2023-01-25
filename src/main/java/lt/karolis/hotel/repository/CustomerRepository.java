57d0f5dc633527a84a441d12970fd12950ef2719
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-26 00:22:56 +0200

    Moved file to right package

--- src/main/java/lt/karolis/hotel/repository/CustomerRepository.java
+++ src/main/java/lt/karolis/hotel/repository/CustomerRepository.java
@@ -1,5 +1,6 @@
-package lt.karolis.hotel.repository.model;
+package lt.karolis.hotel.repository;
 
+import lt.karolis.hotel.repository.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
