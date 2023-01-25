c41a0fbed4fac2d382f4940ceadd988c0179bcd0
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-26 00:23:42 +0200

    Add Many to one and constructor in Reservation POJO

--- src/main/java/lt/karolis/hotel/repository/model/Reservation.java
+++ src/main/java/lt/karolis/hotel/repository/model/Reservation.java
@@ -7,31 +7,37 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.Set;
-
 @Entity
 @Table(name = "reservation")
-@Getter @Setter @NoArgsConstructor @ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Reservation {
 
-    @ManyToMany
-    @JoinTable(
-            name = "Reservation",
-            joinColumns = @JoinColumn(name = "customerid"),
-            inverseJoinColumns = @JoinColumn(name = "roomid"))
-    Set<Rooms> haveReservation;
-    Set<Customers> giveReservation;
-
     @Id
-    @Column(name = "customerid")
-    private Integer customerId;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "reservation_id")
+    private Integer id;
 
-    @Column(name = "roomid")
-    private Integer roomId;
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
 
-    @Column(name = "startdate")
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column(name = "startdate", nullable = false)
     private String startDate;
 
-    @Column(name = "enddate")
+    @Column(name = "enddate", nullable = false)
     private String endDate;
+
+    public Reservation(Customer customer, Room room, String startDate, String endDate) {
+        this.customer = customer;
+        this.room = room;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 }
