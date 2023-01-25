8922b65b235885c50d37697d1eafc5e55eb0a9e9
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-25 11:46:47 +0200

    Add RoomRepository class

---
+++ src/main/java/lt/karolis/hotel/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package lt.karolis.hotel.repository;
+
+import lt.karolis.hotel.repository.model.Room;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room, Integer> {
+}
