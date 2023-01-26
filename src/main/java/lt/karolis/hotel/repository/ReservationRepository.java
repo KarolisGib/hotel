8be8bd66cae622d16f99b1ed13be2f848e653bfb
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-26 09:02:13 +0200

    Add find by Date method in ReservationRepository

--- src/main/java/lt/karolis/hotel/repository/ReservationRepository.java
+++ src/main/java/lt/karolis/hotel/repository/ReservationRepository.java
@@ -3,6 +3,9 @@ package lt.karolis.hotel.repository;
 import lt.karolis.hotel.repository.model.Reservation;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ReservationRepository extends CrudRepository<Reservation, Integer> {
 
+    Optional<Reservation> findByStartDate(String startDate);
 }
