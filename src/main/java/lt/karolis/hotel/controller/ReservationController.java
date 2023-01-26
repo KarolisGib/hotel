ded663f2e7cf74973f6c504f9ded86ffd3bebd7a
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-26 09:01:51 +0200

    Add find by Id method in ReservationController

--- src/main/java/lt/karolis/hotel/controller/ReservationController.java
+++ src/main/java/lt/karolis/hotel/controller/ReservationController.java
@@ -1,9 +1,14 @@
 package lt.karolis.hotel.controller;
 
+import lt.karolis.hotel.repository.model.Reservation;
 import lt.karolis.hotel.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 @Controller //http://localhost:8080/
 @RequestMapping(path = "/reservationmapping") //http://localhost:8080/reservationmapping
@@ -13,4 +18,8 @@ public class ReservationController {
     @Autowired
     private ReservationService reservationService;
 
+    @GetMapping(path = "/reservation/{id}") //http://localhost:8080/reservationmapping/reservation/1
+    public @ResponseBody Reservation getReservationById(@PathVariable Integer id) {
+        return reservationService.getReservationById(id);
+    }
 }
