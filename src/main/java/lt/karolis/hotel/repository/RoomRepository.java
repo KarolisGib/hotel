2e519dff6abb7022614a422bd34eb0eec0701e4f
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-26 09:04:09 +0200

    Add find by city method in RoomRepository

--- src/main/java/lt/karolis/hotel/repository/RoomRepository.java
+++ src/main/java/lt/karolis/hotel/repository/RoomRepository.java
@@ -3,5 +3,9 @@ package lt.karolis.hotel.repository;
 import lt.karolis.hotel.repository.model.Room;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RoomRepository extends CrudRepository<Room, Integer> {
+
+    Optional<Room> findByCity(String city);
 }
